c2f69884b635f75f54df55fdb98c4094a421323c
Andrés Pata Fraile de Manterola <apfm21@Gmail.com>
2023-03-25 12:48:26 -0300

    mejore el proyectito

--- Funciones y Objetos/miniProyecto.js
+++ Funciones y Objetos/miniProyecto.js
@@ -30,12 +30,76 @@ function calcularPromedio (){
     alert(`El promedio total de los ${alumnos} alumnos es: ${sumaTotal/alumnos}`)
 }
 
+const examen = ()=>{
+    alert("Bienvenido al exámen de matemáticas")
+    let nota = 0
+    const corrector= (num1, num2,operacion, rtaAlumno)=>{
+        switch(operacion){
+            case "+":
+                if(num1+num2===rtaAlumno) {
+                    nota += 2
+                }
+                break
+            case "-":
+                if(num1-num2===rtaAlumno) {
+                    nota += 2
+                }
+                break
+            case "*":
+                if(num1*num2===rtaAlumno) {
+                    nota += 2
+                }
+                break
+            case "/":
+                if(num1/num2===rtaAlumno) {
+                    nota += 2
+                }
+            default: console.log("ERROR")
+        }
+        
+    }
+    // esto puede ser su propia funcion que llamo 5 veces
+    let preg1 = Number(promt("Cuánto es 5*8"))
+    corrector(5,8, "*", preg1)
+    let preg2 = Number(promt("Cuánto es 3+4"))
+    corrector(3,4, "+", preg2)
+    let preg3 = Number(promt("Cuánto es 13*2"))
+    corrector(13,2, "*", preg3)
+    let preg4 = Number(promt("Cuánto es 100/4"))
+    corrector(100,4, "/", preg4)
+    let preg5 = Number(promt("Cuánto es 10-3"))
+    corrector(10,3, "-", preg5)
+
+    alert(`Su nota es: ${nota}`)
+}
+
+const preguntaSeleccion=()=>{
+    let eleccion = prompt("Que desea hacer: \n 1 - Calcular promedio \n 2 - Tomar un examen")
+    return Number(eleccion)
+}
+const selector=(eleccion)=>{
+    switch(eleccion){
+        case 1:
+            // calcular promedio
+            calcularPromedio()
+            break
+        case 2:
+            // tomar un examen
+            examen()
+            break
+        default:
+            alert("ingreso un valor inválido")
+    }
+}
+
 const inicializar = ()=>{
     login()
     if(ingreso){
-        calcularPromedio()
-        // calcularSueldomensual()
-        // ingresarReportes()
+        let loop = true
+        do{
+            selector(preguntaSeleccion())
+            loop = confirm("¿Desea continuar?")
+        } while(loop)
     }
 }
 
